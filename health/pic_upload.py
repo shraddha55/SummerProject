fdaa0daed08b47a2d925fb6c4ef6ca91de6665b1
shraddha55 <shrad_1997@yahoo.co.in>
2018-05-16 22:17:10 -0700

    first commit

---
+++ health/pic_upload.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def run_quickstart():
+    # [START vision_quickstart]
+    import io
+    import os
+
+    # Imports the Google Cloud client library
+    # [START migration_import]
+    from google.cloud import vision
+    from google.cloud.vision import types
+    # [END migration_import]
+
+    # Instantiates a client
+    # [START migration_client]
+    client = vision.ImageAnnotatorClient()
+    # [END migration_client]
+
+    # The name of the image file to annotate
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        '/Users/shraddhaagrawal/Desktop/Hackathon/Stanford2018/Sample2.png')
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+    document = response.full_text_annotation
+
+    # Collect specified feature bounds by enumerating all document features
+    for page in document.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    for symbol in word.symbols:
+                        if (feature == FeatureType.SYMBOL):
+                            bounds.append(symbol.bounding_box)
+
+                    if (feature == FeatureType.WORD):
+                        bounds.append(word.bounding_box)
+
+                if (feature == FeatureType.PARA):
+                    bounds.append(paragraph.bounding_box)
+
+            if (feature == FeatureType.BLOCK):
+                bounds.append(block.bounding_box)
+
+        if (feature == FeatureType.PAGE):
+            bounds.append(block.bounding_box)
+
+if __name__ == '__main__':
+    run_quickstart()
